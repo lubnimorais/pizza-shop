c3dc52958f17e9464541a065894f5e7a40c74d24
Lubni Morais <lubni.morais@gmail.com>
2025-03-21 18:34:47 -0300

    feat: start test order status

--- src/components/OrderStatus/order-status.spec.tsx
+++ src/components/OrderStatus/order-status.spec.tsx
@@ -1,3 +1,15 @@
-test('1 + 1 equal 2', () => {
-  expect(1 + 1).toEqual(2);
+import { render } from '@testing-library/react';
+import { OrderStatus } from '.';
+
+describe('Component: Order Status', () => {
+  it('should display the right text based on order status', () => {
+    const wrapper = render(<OrderStatus status="pending" />);
+
+    // MOSTRA NO TERMINAL O HTML GERADO DESTA RENDERIZAÇÃO
+    // wrapper.debug();
+
+    const statusText = wrapper.getByText('Pendente');
+
+    expect(statusText).toBeVisible();
+  });
 });
